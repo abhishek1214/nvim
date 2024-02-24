fa13ef2d781e77ff2be3f6ecad05b9a19c9d3df5
Abhishek Rajamohan <abhishek.rajamohan@wakefit.co>
2024-02-24 19:44:15 +0530

    Added auto pairs plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm size=40 dir=. direction=horizontal name=desktop<cr>")
+vim.keymap.set("n", "<leader>/", "<cmd>ToggleTerm size=40 dir=. direction=horizontal name=desktop<cr>")
+vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle=true<cr>")
