c42b0bd1582b476d90aa54dd50509214410edfdb
Abhishek Rajamohan <abhishek.rajamohan@wakefit.co>
2024-02-25 06:04:35 +0530

    Setup edgyt with floating terminal

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,10 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.edgy" },
+    { import = "lazyvim.plugins.extras.editor.symbols-outline" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
